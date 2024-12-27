5e23c3b4dd9338d9d0f384c0091c3575fa8d1abc
Engin Al <enginal@gmail.com>
2024-12-27 22:31:33 +0300

    Added logging and tracing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
+use bytes::Bytes;
 use clap::Parser;
+use downloader::telemetry::{get_subscriber, init_subscriber};
+use futures_util::stream::Stream;
 use futures_util::StreamExt;
 use reqwest::header::CONTENT_DISPOSITION;
 use tokio::fs::File;
@@ -12,6 +15,7 @@ struct Args {
     link: Vec<String>,
 }
 
+#[tracing::instrument(name = "Get file name from the link", skip(resp))]
 fn get_file_name(resp: &reqwest::Response) -> Result<String> {
     if let Some(header) = resp.headers().get(CONTENT_DISPOSITION) {
         let header_value = header.to_str()?;
@@ -27,26 +31,35 @@ fn get_file_name(resp: &reqwest::Response) -> Result<String> {
         .ok_or_else(|| anyhow!("failed to get file name"))
 }
 
+#[tracing::instrument(name = "Write to a file", skip(stream))]
+async fn write_to_file(
+    filename: &str,
+    mut stream: impl Stream<Item = Result<Bytes, reqwest::Error>> + Unpin,
+) -> Result<()> {
+    let mut file = File::create(&filename).await?;
+
+    while let Some(chunk) = stream.next().await {
+        let chunk = chunk.map_err(|e| anyhow!(e))?;
+        file.write_all(&chunk).await?;
+    }
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    let tracing_subscriber = get_subscriber("downloader-rs".into(), "info".into(), std::io::stdout);
+    init_subscriber(tracing_subscriber);
+
     let args = Args::parse();
     for file_link in args.link.iter() {
-        println!("Downloading: {file_link}");
+        let resp = reqwest::get(file_link)
+            .await
+            .context("Failed to send request")?;
+        let filename = get_file_name(&resp).context("Failed to get file name")?;
 
-        let resp = reqwest::get(file_link).await?;
-        let filename = get_file_name(&resp)?;
-
-        println!("Saving as: {filename}");
-
-        let mut file = File::create(&filename).await?;
-        let mut stream = resp.bytes_stream();
-
-        while let Some(chunk) = stream.next().await {
-            let chunk = chunk?;
-            file.write_all(&chunk).await?;
-        }
-
-        println!("Download complete: {filename}");
+        write_to_file(&filename, resp.bytes_stream())
+            .await
+            .context("Failed to write to a file")?;
     }
     Ok(())
 }
